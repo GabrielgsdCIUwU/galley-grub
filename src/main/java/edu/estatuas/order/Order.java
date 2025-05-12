7e780bd1f8ad85c028c4c31dccc2c8d35bf5e482
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-05-12 14:28:05 +0200

    feat(order): Añadir clase Order

---
+++ src/main/java/edu/estatuas/order/Order.java
@@ -0,0 +1,51 @@
+package edu.estatuas.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.estatuas.interfaces.Comanda;
+import edu.estatuas.items.Item;
+import edu.estatuas.items.ItemFactory;
+
+public class Order implements Comanda {
+
+    private Double total = 0d;
+    private List<Item> listItems = new ArrayList<>();
+
+    @Override
+    public void addItem(String name, Double price) {
+        itemList().add(ItemFactory.getItem(name, price));
+    }
+
+    @Override
+    public void addItem(String name, Double price, String extra) {
+        itemList().add(ItemFactory.getItem(name, price, extra));
+    }
+
+    @Override
+    public int size() {
+        return itemList().size();
+    }
+
+    @Override
+    public List<Item> itemList() {
+        return listItems;
+    }
+
+    @Override
+    public Double getTotal() {
+        return total;
+    }
+
+    @Override
+    public void updateTotal(Double total) {
+        this.total = total;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("\n--- ORDER ---");
+        itemList().stream().forEach(System.out::println);
+    }
+    
+}
